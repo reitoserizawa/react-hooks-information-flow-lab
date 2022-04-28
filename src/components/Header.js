a84de705353dd8f52f83224dd23fbff4ba19f2eb
ReitoSerizawa <s.reitiger@gmail.com>
2022-04-28 16:55:38 -0400

    commit tree 410bdc908efaa915fe042248eaf7240d025da1f7

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Header ({isDarkMode, onDarkModeClick}) {
+    return (
+        <header>
+            <h2>Shopster</h2>
+            <button onClick={onDarkModeClick}>
+            {isDarkMode ? "Dark Mode" : "Light Mode"}
+            </button>
+        </header>
+    )
+}
+
+export default Header
